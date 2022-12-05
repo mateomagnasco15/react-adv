13011a107522c5c221ceea4f8ba785db6aac6eb9
Mateo Magnasco <magnasco.mateo@gmail.com>
2022-12-05 18:15:58 -0300

    control props alternativo

--- src/02-components.patterns/hooks/useShoppingCart.ts
+++ src/02-components.patterns/hooks/useShoppingCart.ts
@@ -9,33 +9,19 @@ export const useShoppingCart = () => {
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
         setShoppingCart(oldShoppingState => {
   
-            const productInCart: ProductInCart = oldShoppingState[product.id] || { ...product, count: 0 };
-  
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingState,
-                    [productInCart.id]: productInCart
-                }
-            }
-            const { [product.id]: toDelete, ...rest } = oldShoppingState;
-            return { ...rest };
-
-        
-  
-            /* if (count === 0) {
+             if (count === 0) {
               const { [product.id]: toDelete, ...rest } = oldShoppingState;
               return rest
             }
             return {
               ...oldShoppingState,
               [product.id]: { ...product, count }
-            } */
+            } 
         })
        
     }
-    return {
-        shoppingCart,
-        onProductCountChange
-    }
+     return {
+            shoppingCart,
+            onProductCountChange
+        }
 }
